0d688bbf32cb65e1a248422c734bf9b3cd9f457b
EC2 Default User <ec2-user@ip-172-31-46-241.ec2.internal>
2022-02-09 07:09:01 +0000

    [Update]3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get "books/new" => "books#new"
-  get 'books/edit'
   get "top" => "homes#top"
   post 'books' => 'books#create'
   get 'books' => 'books#index'
-  get 'books/:id' => 'books#show'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end
